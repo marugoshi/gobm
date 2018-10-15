8915627a45870ed6db6b0921ff8630dd085693ef
Makoto Tokuyama <makoto.tokuyama@gmail.com>
2018-10-15 17:08:36 +0900

    split file

--- presentation/router/router.go
+++ presentation/router/router.go
@@ -3,31 +3,7 @@ package router
 import (
 	"github.com/marugoshi/gobm/presentation/httputils"
 	"net/http"
-	"regexp"
 )
-
-type Routes interface {
-	Data() []Route
-}
-
-type routes struct {
-	data []Route
-}
-
-func (r *routes) Data() []Route {
-	return r.data
-}
-
-func NewRoutes() Routes {
-	routes := &routes{}
-	return routes
-}
-
-type Route struct {
-	Pattern *regexp.Regexp
-	Method  string
-	httputils.Func
-}
 
 type Router struct {
 	Routes
