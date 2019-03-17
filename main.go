08db9b25a44b6b32eaf11bc39ddd2be86408f9d9
Makoto Tokuyama <makoto.tokuyama@gmail.com>
2019-03-18 01:30:34 +0900

    use panic

--- main.go
+++ main.go
@@ -2,21 +2,23 @@ package main
 
 import (
 	"github.com/marugoshi/gobm/presentation/httputils"
-	"github.com/marugoshi/gobm/presentation/router"
-	"github.com/marugoshi/gobm/registry"
-	"log"
+	. "github.com/marugoshi/gobm/presentation/router"
+	. "github.com/marugoshi/gobm/registry"
+	"github.com/marugoshi/gobm/shared/app_log"
 	"net/http"
 )
 
 func main() {
-	registry, err := registry.NewRegistry()
+	registry, err := NewRegistry()
 	if err != nil {
-		log.Fatalf("Could not create DB: %s\n", err.Error())
+		app_log.Fatalf("panic: %#v", err)
+		panic("could not start.")
 	}
 	defer registry.DB.Close()
-	router := router.NewRouter(registry, httputils.ContentTypeTextHtml)
+	router := NewRouter(registry, httputils.ContentTypeTextHtml)
 	err = http.ListenAndServe(":8080", router)
 	if err != nil {
-		log.Fatalf("Could not start: %s\n", err.Error())
+		app_log.Fatalf("panic: %#v", err)
+		panic("could not start.")
 	}
 }
